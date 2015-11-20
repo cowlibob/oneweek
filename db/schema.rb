8592222cd113c0d4ab548c751e458b0c16faceb6
JamesCowlishaw <james.cowlishaw@white-space.co.uk>
2015-11-20 13:19:04 +0000

    Use Postgres for Heroku.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151117201238) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "routes", force: :cascade do |t|
     t.string   "name"
     t.text     "xml"
